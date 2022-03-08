f49a941b0969c7ec57ad8ab95c75834cc2b82710
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-09 06:49:47 +0700

    login and logout handler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
+import {useDispatch, useSelector} from 'react-redux';
+import { authActions } from '../store';
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
   return (
     <header className={classes.header}>
-      <h1>Redux Auth</h1>
+    <h1>Redux Auth</h1>
+    {isAuth && (
       <nav>
         <ul>
           <li>
@@ -13,10 +23,12 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
       </nav>
+    )
+    }
     </header>
   );
 };
